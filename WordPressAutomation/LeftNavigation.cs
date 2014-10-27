dbe25094384ea19816c0a17fcdb9b57ac2c78037
Brad Rosen <brosen@mdsol.com>
2014-10-27 11:05:35 -0400

    refactor 2

---
+++ WordPressAutomation/LeftNavigation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace WordPressAutomation
+{
+    //Lots of nested classes
+    public class LeftNavigation
+    {
+        public class Posts
+        {
+            public class AddNew
+            {
+                public static void Select()
+                {
+                    MenuSelector.Select("menu-posts", "Add New");
+                }
+            }
+        }
+        public class Pages
+        {
+            public class AllPages
+            {
+                public static void Select()
+                {
+                    MenuSelector.Select("menu-pages", "Add Pages");
+                }
+            }
+        }
+    }
+    public class MenuSelector
+	{
+
+        public static void Select(string topLevelMenuId, string subMenuLinkText)
+        {
+            Driver.Instance.FindElement(By.Id(topLevelMenuId)).Click();
+            Driver.Wait(TimeSpan.FromSeconds(1));
+            Driver.Instance.FindElement(By.LinkText(subMenuLinkText)).Click();
+        }
+    } 
+}
--- WordPressAutomation/NewPostPage.cs
+++ WordPressAutomation/NewPostPage.cs
@@ -71,34 +71,4 @@ namespace WordPressAutomation
             Driver.Instance.FindElement(By.Id("publish")).Click();
         }
     }
-
-    public class LeftNavigation
-    {
-        public class Posts
-        {
-            public class AddNew
-            {
-                public static void Select()
-                {
-                    var menuPosts = Driver.Instance.FindElement(By.Id("menu-posts"));
-                    menuPosts.Click();
-
-                    var addNew = Driver.Instance.FindElement(By.LinkText("Add New"));
-                    addNew.Click();
-                }
-            }
-        }
-        public class Pages
-        {
-            public class AllPages
-            {
-                public static void Select()
-                {
-                    Driver.Instance.FindElement(By.Id("menu-pages")).Click();
-                    Driver.Wait(TimeSpan.FromSeconds(1));
-                    Driver.Instance.FindElement(By.LinkText("All Pages")).Click();
-                }
-            }
-        }
-    }
 }
